24f8b916962f0156ea281e5770dec9078094b12d
Leonardo-Eiras <leonardo@am4.com.br>
2014-07-09 12:47:02 -0300

    Cópia Inicial
    Primeiro Commit.

---
+++ page/_scripts.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="../bower_components/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="../scripts/scripts.min.js"></script>
---
+++ page/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xml:lang="pt-br" lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=810px">
+<meta name="author" content="AM4 - A Internet de Resultados" />
+<meta name="resource-type" content="document"/>
+<meta name="content-language" content="pt-br"/> 
+<title>Modelo LandingPage Hangout</title>
+<?php include('_css.php'); ?>
+
+</head>
+<body>
+<?php include('_analytics.php'); ?>
+<form runat="server">
+	<div class="main-header">
+		<div class="info-box">
+			<p class="title">Hangout</p>
+			<p>Conhe&ccedil;a nosso escrit&oacute;rio na Fundi&ccedil;&atilde;o Progresso-RJ. Mais carioca imposs&iacute;�vel!</p>
+		</div>
+
+		<p class="platform-title">Plataforma de v&iacute;deo<br><span>AO VIVO</span></p>
+	</div>
+	
+	<iframe src="//player.vimeo.com/video/51168196" width="765" height="435" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+	<a href="http://www.am4.com.br/" target="_blank">
+		<img src="../images/footer.jpg" height="98" width="792" alt="Tenha maiores informações sobre a empresa" class="footer" border="0">
+	</a>
+</form>
+</body>
+</html>
